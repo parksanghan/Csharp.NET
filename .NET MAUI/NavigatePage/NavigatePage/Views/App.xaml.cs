061f4396085132cbe038b8defac0948eb7e7fb94
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-03-22 00:22:58 +0900

    upload nav
    nav

---
+++ .NET MAUI/NavigatePage/NavigatePage/Views/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NavigatePage.Views;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ .NET MAUI/NavigatePage/NavigatePage/Views/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigatePage.Views;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
