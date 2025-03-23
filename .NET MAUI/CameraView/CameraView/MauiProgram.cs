0ce94d3a990c0601959d26899ba649243348a3e7
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-03-23 17:34:42 +0900

    Camera view check
    Camera view check

---
+++ .NET MAUI/CameraView/CameraView/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Maui;
+ 
+using Microsoft.Extensions.Logging;
+
+namespace CameraView
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<Views.App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitCamera()
+                .UseMauiCommunityToolkitMediaElement()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
