278e545a1a5ff7a6a5494ffdd66dec2b3366137d
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-04-02 22:51:51 +0900

    function mediapicker
    function mediapicker

--- .NET MAUI/MyCamera2Preview/MyCamera2Preview/obj/Debug/net9.0-android/MyCamera2Preview.AssemblyInfo.cs
+++ .NET MAUI/MyCamera2Preview/MyCamera2Preview/obj/Debug/net9.0-android/MyCamera2Preview.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCamera2Preview")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+01a81918faaf43452a35270b45ee8ef7b2375a9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4011a66ad597143b8cf5587089489130edbe8628")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCamera2Preview")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCamera2Preview")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
