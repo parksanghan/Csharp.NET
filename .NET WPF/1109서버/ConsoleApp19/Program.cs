8b06ca82d36f0b2a50733963d3aedda504c0d6a8
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-05-27 19:43:24 +0900

    server

---
+++ .NET WPF/1109서버/ConsoleApp19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Control con = Control.Instance;
+
+            con.Run();
+            Console.ReadLine();
+            
+        }
+    }
+}
