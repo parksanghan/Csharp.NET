73922e68cf57b43c5a035dcdf2c778600b37ebf5
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-07-10 23:35:00 +0900

    fin

--- Data Structure/Stack/Stack/Stack.cpp
+++ Data Structure/Stack/Stack/Stack.cpp
@@ -56,7 +56,7 @@ public:
     }
     void print_all() const {
         std::cout << "top = " << top << std::endl;
-        for (size_t i = 0; i < top; i++)
+        for (size_t i = 0; i <= top; i++)
         {
             std::cout << arr[i] << std::endl;
             
