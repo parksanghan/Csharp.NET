1a28140ccfa682570498b09c81fbad3799438392
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-07-28 21:14:39 +0900

    IIncremental Generator

---
+++ CodeGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSHARP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Calculator cal =  new Calculator();
+    }
+}
