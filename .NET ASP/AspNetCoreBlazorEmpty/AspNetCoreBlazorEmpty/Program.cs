3cc3de15c16df7792eeb3f6a577617be5fe46878
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-08-05 00:20:47 +0900

    asp setting init

--- .NET ASP/AspNetCoreBlazorEmpty/AspNetCoreBlazorEmpty/Program.cs
+++ .NET ASP/AspNetCoreBlazorEmpty/AspNetCoreBlazorEmpty/Program.cs
@@ -6,16 +6,18 @@ builder.Services.AddDbContext<BlazorTDBContext>(opts =>
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]); // db연결
     opts.EnableSensitiveDataLogging(true);
 });
-builder.Services.AddControllersWithViews(); 
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews(); // Controller +view 사용 설정
+builder.Services.AddRazorPages(); // Razor Pages 사용 설정  .cshtml
 
-var app = builder.Build();
+var app = builder.Build(); // 빌더 설정으로 app 객체 생성
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); // MapControllers  , MapGet 둘다 공존해도도힘
 
-app.UseStaticFiles();
-app.MapControllers();
+app.UseStaticFiles();// 정적 라우팅 설정 
+app.MapControllers(); // 컨트롤러 기반 요청 처리 설정
 app.MapControllerRoute("controllers", "controllers/{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+// MVC 라우팅 집적 지정
+app.MapRazorPages();// Pages 기반  Razor 라우팅 활성
+//@page 디렉티브를 가진 .cshtml 파일 처리됨
 app.Run();
 // 빈프로젝트 
