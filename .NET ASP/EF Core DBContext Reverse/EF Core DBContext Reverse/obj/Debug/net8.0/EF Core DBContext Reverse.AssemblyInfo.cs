411cb45c8e1985a1adc4962246b6fa37f7f94d9c
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-08-07 14:53:19 +0900

    wwwroot

--- .NET ASP/EF Core DBContext Reverse/EF Core DBContext Reverse/obj/Debug/net8.0/EF Core DBContext Reverse.AssemblyInfo.cs
+++ .NET ASP/EF Core DBContext Reverse/EF Core DBContext Reverse/obj/Debug/net8.0/EF Core DBContext Reverse.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF Core DBContext Reverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98670632702010d5d13a7e61ee2057716712901b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e01569cbcca7fc82af90e794e78250b81234f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF Core DBContext Reverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF Core DBContext Reverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
