8bfd5256d1238ed443826d0f74badcd97ce1e86c
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-08-10 09:06:42 +0900

    DataModel

--- .NET ASP/EF Core Entity DBContext/EF Core Entity DBContext/obj/Debug/net8.0/EF Core Entity DBContext.AssemblyInfo.cs
+++ .NET ASP/EF Core Entity DBContext/EF Core Entity DBContext/obj/Debug/net8.0/EF Core Entity DBContext.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF Core Entity DBContext")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f350292b31ee06871139ef8d0d0f4a74f418af2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa815468cb5b6856018e04cd0a8ba58728bdeed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF Core Entity DBContext")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF Core Entity DBContext")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
