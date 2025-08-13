7420d4026f73bcd85bf5c371887dce4dcad53777
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-08-13 22:00:21 +0900

    set

--- .NET ASP/BlazorServerSignalR/BlazorServerSignalR/Program.cs
+++ .NET ASP/BlazorServerSignalR/BlazorServerSignalR/Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<NorthwindContext>(opts => {
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:NorthwindConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.UseStaticFiles();
+app.MapControllers();
+app.MapControllerRoute("controllers", "controllers/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+app.MapBlazorHub();
