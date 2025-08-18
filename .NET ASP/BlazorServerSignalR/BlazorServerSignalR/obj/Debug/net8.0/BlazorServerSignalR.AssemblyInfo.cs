5aced24c36af75e44aeac9d926fc0bd3c19af6d2
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-08-18 21:29:53 +0900

    usiing component razor

--- .NET ASP/BlazorServerSignalR/BlazorServerSignalR/obj/Debug/net8.0/BlazorServerSignalR.AssemblyInfo.cs
+++ .NET ASP/BlazorServerSignalR/BlazorServerSignalR/obj/Debug/net8.0/BlazorServerSignalR.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorServerSignalR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bdfc805799d13bb5e3882456d6aadaf5dcd74f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1c0ed7f3734688c70f1f12c6544d61e323e102")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorServerSignalR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorServerSignalR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
