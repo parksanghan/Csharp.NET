7cba6d023e33ca23fa98e6e64f2447a6eaa5aa23
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-08-27 17:48:43 +0900

    Razor Component split

---
+++ .NET ASP/BlazorServerSignalR/BlazorServerSignalR/Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,12 @@
+﻿using BlazorServerSignalR.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerSignalR.Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public BlazorTDBContext? TDBContext { get; set; }    
+        public IEnumerable<string> Names =>TDBContext.Products.Select(n=>n.ProductName) ?? Enumerable.Empty<string>();  
+    }
+}
