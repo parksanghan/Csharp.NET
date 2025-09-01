cd2f98ac88307053c093599ced06cc30194fda06
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-09-01 21:21:20 +0900

    two way binding

--- .NET ASP/BlazorServerSignalR/BlazorServerSignalR/obj/Debug/net8.0/BlazorServerSignalR.AssemblyInfo.cs
+++ .NET ASP/BlazorServerSignalR/BlazorServerSignalR/obj/Debug/net8.0/BlazorServerSignalR.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorServerSignalR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34cfe632d8e17b744a7b48bd605f955b70a2ecab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58e9473e53bce54b3566d1aacf8188303f40002")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorServerSignalR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorServerSignalR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
