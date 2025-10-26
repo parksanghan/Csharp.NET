518c4adacff997421ac7acfc516f7feb76b9fbbe
박상한 <68534093+parksanghan@users.noreply.github.com>
2025-10-26 14:02:39 +0900

    client

---
+++ .NET C# Language Network/TCPClientSkleton/TCPServerSkleton/MSocket/MSocketProperty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerSkleton.MSocket
+{
+    public enum MSocketProperty
+    {
+        Init=0,
+        Connect=1,
+        Disconnect=2,
+        Send=3,
+        Receive=4,
+        Accept=5,
+        Stop=6
+    }
+}
---
+++ .NET C# Language Network/TCPClientSkleton/TCPServerSkleton/MSocket/MSocketServer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TCPServerSkleton.MSocket;
+
+namespace ConsoleApp3.MSocket
+{
+
+    public class MSocketServer
+    {
+        private readonly Socket serverSocket;
+        private CancellationTokenSource? cts; // 비동기 관리 객체 
+        private readonly List<Socket> clients;
+        // 대리자 타입 정의
+        private delegate void LogDelegate(Socket clientSocket, MSocketProperty mSocketProperty); //  일반로그 대리자
+        private delegate void ErrorDelegate(Socket clientSocket, Exception ex, MSocketProperty mSocketProperty); // 에러로그 대리자
+
+        // 대리자 인스턴스 선언
+        private LogDelegate logDelegate;
+        private ErrorDelegate errorDelegate;
+
+        // 함수포인터 대리자 외부 초기화
+        public void SetDelegate(Action<Socket, MSocketProperty> logAction, Action<Socket, Exception, MSocketProperty>? errAction)
+        {
+            this.logDelegate = (sock, state) => logAction(sock, state);
+            if (errAction != null) this.errorDelegate = (sock, ex, state) => errAction(sock, ex, state);
+        }
+        // 함수포인터 대리자 내부 초기화 
+        private void SetDelegate()
+        {
+            this.logDelegate = (sock, state) =>
+            {
+                Console.WriteLine($"[LOG] Client {sock.RemoteEndPoint} - State: {state.ToString()}");
+            };
+            this.errorDelegate = (sock, ex, state) =>
+            {
+                Console.WriteLine($"[ERROR] Client {sock.RemoteEndPoint} - State: {state.ToString()} - ErrorType: {ex.Message}");
+
+            };
+        }
+        public MSocketServer(Socket serverSocket)
+        {
+            this.serverSocket = serverSocket ?? throw new ArgumentNullException(nameof(serverSocket));
+            SetDelegate(); // 기본 로그 대리자 설정
+            clients = new List<Socket>();
+
+            logDelegate(serverSocket, MSocketProperty.Init);
+
+
+        }
+        public void RemoveClient(Socket socket)
+        {
+            lock (clients)
+            {
+                if (clients.Contains(socket))
+                {
+                    clients.Remove(socket);
+                    socket.Close();
+                    logDelegate(socket, MSocketProperty.Disconnect);
+
+                }
+            }
+        }
+        public void AddClient(Socket socket)
+        {
+            lock (clients)
+            {
+                if (!clients.Contains(socket))
+                {
+                    clients.Add(socket);
+                    logDelegate(socket, MSocketProperty.Connect);
+                }
+            }
+
+        }
+        public static MSocketServer Create()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            return new MSocketServer(serverSocket);
+        }
+        public void SocketInit(string address)
+        {
+            serverSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            serverSocket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+            serverSocket.Bind(System.Net.IPEndPoint.Parse(address));
+
+        }
+        public void StartListening(int backlog = 100)
+        {
+            serverSocket.Listen(backlog);
+            Console.WriteLine("Server is listening...");
+            cts = new CancellationTokenSource();// 비동기 관리 객체 초기화
+
+            Task.Run(() => AcceptLoopClient(cts.Token));
+        }
+        public void AcceptLoopClient(CancellationToken token)
+        {
+            logDelegate(serverSocket, MSocketProperty.Accept);
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    Socket clientSocket = serverSocket.Accept();
+                    logDelegate(clientSocket, MSocketProperty.Accept);
+                    AddClient(clientSocket);
+                    // 호출 뒤에 각 쓰레드에 할당
+                    Thread thread = new Thread(() => HandleClient(clientSocket))
+                    {
+                        IsBackground = true
+                    };
+                    thread.Start();
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine($"Socket exception: {ex.Message}");
+                    errorDelegate(serverSocket, ex, MSocketProperty.Accept);
+
+                }
+                catch (ObjectDisposedException ex)
+                {
+                    errorDelegate(serverSocket, ex, MSocketProperty.Accept);
+                    // 서버 소켓이 닫혔을 때 발생할 수 있음
+                    break;
+                }
+            }
+
+        }
+        private void HandleClient(Socket clientSocket)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+                    int received = clientSocket.Receive(buffer);
+                    logDelegate(clientSocket, MSocketProperty.Receive);
+                    if (received == 0) break;
+
+                    string msg = Encoding.UTF8.GetString(buffer, 0, received);
+                    Console.WriteLine($"[Client {clientSocket.RemoteEndPoint}] {msg}");
+
+                    // 에코 응답
+                    byte[] data = Encoding.UTF8.GetBytes("Echo: " + msg);
+                    clientSocket.Send(data);
+                    logDelegate(clientSocket, MSocketProperty.Send);
+                }
+            }
+            catch (Exception ex)
+            {
+                errorDelegate(clientSocket, ex, MSocketProperty.Receive);
+                Console.WriteLine($"[WARN] Client error: {ex.Message}");
+                RemoveClient(clientSocket);
+            }
+            finally
+            {
+                logDelegate(clientSocket, MSocketProperty.Disconnect);
+                clientSocket.Close();
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                logDelegate(serverSocket, MSocketProperty.Stop);
+                List<Socket> snapshot;
+                lock (clients) snapshot= new List<Socket>(clients);
+                foreach (var client in snapshot)
+                {
+                    logDelegate(client, MSocketProperty.Disconnect);
+                    RemoveClient(client);
+                }
+                serverSocket.Close();
+                lock(clients)clients.Clear();
+            }
+            catch (Exception ex)
+            {
+                errorDelegate(serverSocket, ex, MSocketProperty.Stop);
+            }
+            finally
+            {
+                lock (clients) clients.Clear(); 
+            }
+        }
+    }
+}
