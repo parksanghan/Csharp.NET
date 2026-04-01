1f677edced165589ff816ef9089b679e68fb1505
박상한 <68534093+parksanghan@users.noreply.github.com>
2026-04-01 23:17:32 +0900

    init

---
+++ .NET C# Language Network/CUDPServer/Socket/CUDPServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUDPServer.Socket
+{
+    internal class CUDPServer
+    {
+
+        public UdpClient udpServer;
+
+        public CUDPServer(int port)
+        {
+            udpServer = new UdpClient(port);
+            IPEndPoint serverEp = new IPEndPoint(IPAddress.Any, port);
+            udpServer.
+        }
+    }
+}
