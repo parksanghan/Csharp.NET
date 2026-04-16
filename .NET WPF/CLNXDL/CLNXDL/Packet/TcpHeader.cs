ee8cc13f114f0033a02c38010e28d304695af7c2
박상한 <68534093+parksanghan@users.noreply.github.com>
2026-04-16 17:00:10 +0900

    Define TCP Header

---
+++ .NET WPF/CLNXDL/CLNXDL/Packet/TcpHeader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLNXDL.Packet
+{
+    public class TcpHeader
+    {
+        public const int MinHeaderSize = 20;
+        public ushort SourcePort { get; set; }
+        public ushort DestinationPort { get; set; }
+        public uint SequenceNumber { get; set; }
+        public uint AcknowledgmentNumber { get; set; }
+        /// <summary>
+        /// 4 - bit 값   => TCP Header Length
+        /// 기본 20바이트 = 5
+        /// </summary>
+        public byte DataOffset { get; set; } = 5;
+        public bool Fin { get; set; }
+        public bool Syn { get; set; }
+        public bool Rst { get; set; }
+        public bool Psh { get; set; }
+        public bool Ack { get; set; }
+        public bool Urg { get; set; }
+        public bool Ece { get; set; }
+        public bool Cwr { get; set; }
+
+        public ushort WindowSize { get; set; }
+        public ushort Checksum { get; set; }
+        public ushort UrgentPointer { get; set; }
+
+        public byte[] Options { get; set; } = Array.Empty<byte>();
+        public int HeaderLength => DataOffset * 4;
+
+        //public byte[] Tobytes()
+        //{
+        //    int  headerlength =  HeaderLength;
+        //    if (headerlength < MinHeaderSize) throw new InvalidOperationException("Tcp headersize must be satisfied with 20 bytes..");
+        //    if(headerlength > 60) throw new InvalidOperationException($"Invalid header length: {headerlength}..");
+        //    int optionlength = Options == null ? 0 : Options.Length;
+        //    if(MinHeaderSize + optionlength != headerlength)throw new InvalidOperationException($"Invalid header length: {headerlength}..");
+        //    byte[] buffer = new byte[headerlength];
+             
+        //}
+    }
+}
---
+++ .NET WPF/CLNXDL/CLNXDL/Packet/TcpSegment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLNXDL.Packet
+{
+    internal class TcpSegment
+    {
+    }
+}
