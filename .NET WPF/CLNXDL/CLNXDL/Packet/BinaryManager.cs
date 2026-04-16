ccc039f38eb45f9f646c3e498a12f350f096fcf9
박상한 <68534093+parksanghan@users.noreply.github.com>
2026-04-16 18:44:54 +0900

    Update BinaryManager.cs

--- .NET WPF/CLNXDL/CLNXDL/Packet/BinaryManager.cs
+++ .NET WPF/CLNXDL/CLNXDL/Packet/BinaryManager.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CLNXDL.Packet
 {
-    internal class BinaryManager
+    public static class BinaryManager
     {
+        /// <summary>
+        /// Little Endian 방식 바이너리 모듈  
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        #region Little Endian  
+        public static void  WriteUInt16BE(this BinaryWriter writer, ushort value)
+        {
+            writer.Write(value);
+        }
+        #endregion
+        /// <summary>
+        ///  Big Endian 방식 바이너리 모듈 
+        /// </summary>
+        #region Big Endian  
+        public static void test()
+        {
+
+        }
+        #endregion
     }
 }
