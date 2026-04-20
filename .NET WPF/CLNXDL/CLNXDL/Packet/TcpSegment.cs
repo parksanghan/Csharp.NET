01f915a94f382636ec0e878dbc0b4903ef0c34d6
박상한 <68534093+parksanghan@users.noreply.github.com>
2026-04-20 17:09:19 +0900

    Update TcpSegment.cs

--- .NET WPF/CLNXDL/CLNXDL/Packet/TcpSegment.cs
+++ .NET WPF/CLNXDL/CLNXDL/Packet/TcpSegment.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CLNXDL.Packet;
+using System;
 
-namespace CLNXDL.Packet
+public sealed class TcpSegment
 {
-    internal class TcpSegment
+    public TcpHeader Header { get; set; }
+    public byte[] Payload { get; set; }
+
+    public TcpSegment()
     {
+        Header = new TcpHeader();
+        Payload = new byte[0];
+    }
+
+    public byte[] ToBytes()
+    {
+        byte[] headerBytes = Header.ToBytes();
+
+        int payloadLength = Payload == null ? 0 : Payload.Length;
+        byte[] result = new byte[headerBytes.Length + payloadLength];
+
+        Buffer.BlockCopy(headerBytes, 0, result, 0, headerBytes.Length);
+
+        if (payloadLength > 0)
+        {
+            Buffer.BlockCopy(Payload, 0, result, headerBytes.Length, payloadLength);
+        }
+
+        return result;
+    }
+
+    public static TcpSegment FromBytes(byte[] buffer)
+    {
+        if (buffer == null)
+            throw new ArgumentNullException("buffer");
+
+        if (buffer.Length < TcpHeader.MinHeaderSize)
+            throw new ArgumentException("Invalid TCP segment.");
+
+        TcpHeader header = TcpHeader.FromBytes(buffer);
+
+        int headerLength = header.HeaderLength;
+        int payloadLength = buffer.Length - headerLength;
+
+        byte[] payload = new byte[payloadLength];
+
+        if (payloadLength > 0)
+        {
+            Buffer.BlockCopy(buffer, headerLength, payload, 0, payloadLength);
+        }
+
+        return new TcpSegment
+        {
+            Header = header,
+            Payload = payload
+        };
     }
 }
