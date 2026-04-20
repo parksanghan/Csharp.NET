a9dd018185002242fee2bb1797c413d22435aa3e
박상한 <68534093+parksanghan@users.noreply.github.com>
2026-04-20 16:40:36 +0900

    Endian updates

--- .NET WPF/CLNXDL/CLNXDL/Packet/BinaryManager.cs
+++ .NET WPF/CLNXDL/CLNXDL/Packet/BinaryManager.cs
@@ -14,10 +14,25 @@ namespace CLNXDL.Packet
         /// </summary>
         /// <param name="writer"></param>
         /// <param name="value"></param>
-        #region Little Endian  
+        #region Little Endian Writer
         public static void  WriteUInt16BE(this BinaryWriter writer, ushort value)
         {
-            writer.Write(value);
+            writer.Write((byte)(value >> 8));
+            writer.Write(((byte)value));
+        }
+        public static void WriteUInt32BE(this BinaryWriter writer, ushort value)
+        {
+            writer.Write((byte)(value) >> 24);
+            writer.Write((byte)(value) >> 16);
+            writer.Write((byte)(value) >> 8);
+            writer.Write(((byte)value));
+        }
+        #endregion
+
+        #region Little Endian Writer
+        public static void ReadUInt16BE(this BinaryReader reader)
+        {
+
         }
         #endregion
         /// <summary>
