7ccdc5928322e6020bb3c239473f0afcc830e0d7
박상한 <68534093+parksanghan@users.noreply.github.com>
2026-07-05 23:15:47 +0900

    IIncremental

--- Code-Generator/src/Project1.App/Program.cs
+++ Code-Generator/src/Project1.App/Program.cs
@@ -1,13 +1,51 @@
 using Generated.Communication;
 
 Console.WriteLine($"Generated routes for {CommunicationRoutes.ProjectName}");
+Console.WriteLine();
+Console.WriteLine("Outgoing routes");
 
-foreach (var route in CommunicationRoutes.All)
+foreach (var route in CommunicationRoutes.Outgoing)
 {
-    var endpoint = route.Port is null
-        ? $"{route.Protocol} {route.Host}"
-        : $"{route.Protocol} {route.Host}:{route.Port}";
+    Console.WriteLine($"{route.Target} -> {route.Endpoint} | {route.Description}");
+}
 
-    Console.WriteLine($"{route.Target} -> {endpoint} | {route.Description}");
+Console.WriteLine();
+Console.WriteLine("Incoming routes");
+
+foreach (var route in CommunicationRoutes.Incoming)
+{
+    Console.WriteLine($"{route.Source} -> {route.Endpoint} | {route.Description}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Generated network API examples");
+
+foreach (var route in CommunicationRoutes.Outgoing)
+{
+    if (route.Protocol == Protocol.Tcp)
+    {
+        Console.WriteLine($"await CommunicationNetwork.ConnectAsync(\"{route.Target}\");");
+        Console.WriteLine($"await CommunicationNetwork.SendAsync(\"{route.Target}\", \"hello\");");
+        Console.WriteLine($"var reply = await CommunicationNetwork.ReceiveAsync(\"{route.Target}\");");
+        Console.WriteLine($"CommunicationNetwork.Disconnect(\"{route.Target}\");");
+    }
+    else
+    {
+        Console.WriteLine($"await CommunicationNetwork.SendUdpAsync(\"{route.Target}\", \"hello\");");
+    }
+}
+
+foreach (var route in CommunicationRoutes.Incoming)
+{
+    if (route.Protocol == Protocol.Tcp)
+    {
+        Console.WriteLine($"await CommunicationNetwork.StartReceiveServerAsync(\"{route.Source}\");");
+        Console.WriteLine($"var message = await CommunicationNetwork.ReceiveFromAsync(\"{route.Source}\");");
+        Console.WriteLine($"CommunicationNetwork.StopReceiveServer(\"{route.Source}\");");
+    }
+    else
+    {
+        Console.WriteLine($"var message = await CommunicationNetwork.ReceiveUdpFromAsync(\"{route.Source}\");");
+    }
 }
 
